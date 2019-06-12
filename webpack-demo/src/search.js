084f318df14c4c4f7b6e6fe8e7d096cb4b222b17
Duan Yupeng <fengdurant@163.com>
2019-06-12 22:46:58 +0800

    add webpack dist react module

---
+++ webpack-demo/src/search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class Search extends React.Component {
+
+  render() {
+    return <div> Search Text </div>
+  }
+}
+
+ReactDom.render( 
+  <Search/> ,
+  document.getElementById('root')
+)
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,12 +2,20 @@ const path = require('path')
 const MyPlugin = require('./src/myPlugin.js')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: './src/index.js',
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /.js$/,
+      use: 'babel-loader'
+    }]
   },
   plugins: [
     new MyPlugin()
