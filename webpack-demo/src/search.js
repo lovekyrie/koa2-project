c99fe63448ebf75f622f503cef8d0cad6ccb57ae
Duan Yupeng <fengdurant@163.com>
2019-06-16 22:38:42 +0800

    add webpack-dev-middleware

--- webpack-demo/src/search.js
+++ webpack-demo/src/search.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import logo from './images/logo.png'
+import './search.less'
 
 class Search extends React.Component {
 
   render() {
-    return <div> Search Text </div>
+    return <div className="search-text"> 搜索文字的内容
+      <img src={logo} />
+    </div>
   }
 }
 
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,23 +1,51 @@
 const path = require('path')
+const webpack=require('webpack')
 const MyPlugin = require('./src/myPlugin.js')
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     index: './src/index.js',
     search: './src/search.js'
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    publicPath:'/dist/'
   },
   module: {
     rules: [{
       test: /.js$/,
       use: 'babel-loader'
+    },{
+      test:/.css$/,
+      use:[
+        'style-loader',
+        'css-loader'
+      ]
+    },{
+      test:/.less$/,
+      use:[
+        'style-loader',
+        'css-loader',
+        'less-loader'
+      ]
+    },{
+      test:/\.(png|jpg|svg|gif)$/,
+      use:[
+        {
+          loader:'url-loader',
+          options:{
+            limit:10240
+          }
+        }
+      ]
+    },{
+      test:/\.(woff|woff2|eot|ttf|otf)$/,
+      use:'file-loader'
     }]
   },
   plugins: [
-    new MyPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
