3d0a6506cf20934367ecce662de8ef475136c466
Duan Yupeng <fengdurant@163.com>
2019-06-17 17:42:32 +0800

    setting change by network

--- webpack-demo/dev-server.js
+++ webpack-demo/dev-server.js
@@ -1,6 +1,7 @@
 const koa=require('koa')
 const webpack=require('webpack')
 const webpackDevMiddleware=require('webpack-dev-middleware')
+const server = require("koa-static");
 
 const app=new koa()
 const config=require('./webpack.config')
@@ -10,8 +11,12 @@ app.use(webpackDevMiddleware(compiler,{
   publicPath:config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler))
+app.use(require('webpack-hot-middleware')(compiler,{
+  log:console.log,path:'/__webpack_hmr',heartbeat:10*1000
+}))
 
-app.listen(3000,function(){
+app.use(server(config.output.path))
+
+app.listen(3000,()=>{
   console.log('Example app listening on port 3000.\n');
 })
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,51 +1,45 @@
-const path = require('path')
-const webpack=require('webpack')
-const MyPlugin = require('./src/myPlugin.js')
+const path = require("path");
+const webpack = require("webpack");
+const MyPlugin = require("./src/myPlugin.js");
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    search: './src/search.js'
-  },
+  mode: "development",
+  entry: ["webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000", "./src/index.js", "./src/search.js"],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath:'/dist/'
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/"
   },
   module: {
-    rules: [{
-      test: /.js$/,
-      use: 'babel-loader'
-    },{
-      test:/.css$/,
-      use:[
-        'style-loader',
-        'css-loader'
-      ]
-    },{
-      test:/.less$/,
-      use:[
-        'style-loader',
-        'css-loader',
-        'less-loader'
-      ]
-    },{
-      test:/\.(png|jpg|svg|gif)$/,
-      use:[
-        {
-          loader:'url-loader',
-          options:{
-            limit:10240
+    rules: [
+      {
+        test: /.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10240
+            }
           }
-        }
-      ]
-    },{
-      test:/\.(woff|woff2|eot|ttf|otf)$/,
-      use:'file-loader'
-    }]
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader"
+      }
+    ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
-}
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
